494a122cd9c0c20ab48664bcd0def0812b35c847
jgrassia <jgrassia@atomicmouse.com>
2019-05-07 09:31:46 -0400

    Modify js to wait for page load before adding target attribute

--- src/resources/script.js
+++ src/resources/script.js
@@ -1,3 +1,14 @@
-(function($) {
-    $(".instructions a").attr("target", "_blank");
+/* global jQuery */
+
+(function ($) {
+    // Set up interval check to see when Neo field is done loading
+    // (it adds style attribute to body)
+    var checkCount = 0;
+    var loadCheck = setInterval(function () {
+        checkCount++;
+        if ($('body')[0].hasAttribute('style') || (checkCount > 10)){
+            clearInterval(loadCheck);
+            $(".instructions a").attr("target", "_blank");
+        }
+    }, 1000);
 })(jQuery);
